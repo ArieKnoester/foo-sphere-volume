f3dcd6e195d098340ac78db2b5cf37267a9a2bf2
Arie Knoester <arikstr@gmail.com>
2024-06-02 17:27:50 -0500

    Expose research algorithm as top level import

--- src/foo_sphere_volume/__init__.py
+++ src/foo_sphere_volume/__init__.py
@@ -7,5 +7,6 @@ foo-sphere-volume: Calculate the complex (yet fictitious) Foo, et al. parameteri
 from __future__ import annotations
 
 from ._version import version as __version__
+from .sphere_volume import sphere_volume_given_radius
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "sphere_volume_given_radius"]
